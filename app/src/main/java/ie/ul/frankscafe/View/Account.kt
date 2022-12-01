428695f578e7380715cabab140c763c31811e75d
EvanMcKie <evanmckie@outlook.com>
2022-12-01 23:49:03 +0000

    User can toggle notifications, fixed numbering issues

--- app/src/main/java/ie/ul/frankscafe/View/Account.kt
+++ app/src/main/java/ie/ul/frankscafe/View/Account.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
+import android.widget.ToggleButton
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import ie.ul.frankscafe.Model.entity.UserEntity
 import ie.ul.frankscafe.R
+import ie.ul.frankscafe.Services.CurrentUser
+import ie.ul.frankscafe.ViewModel.UserViewModel
+import ie.ul.frankscafe.databinding.ActivityMainBinding
 
 class Account : Fragment(R.layout.account){
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater!!.inflate(R.layout.account, container, false)
         val regButton = view.findViewById<AppCompatButton>(R.id.pastorders)
+        val toggle = view.findViewById<Switch>(R.id.notificationToggle)
+        val user = CurrentUser.getuser()
+
         regButton.setOnClickListener{
 
             val fm = (activity as FragmentActivity).supportFragmentManager
@@ -22,6 +32,15 @@ class Account : Fragment(R.layout.account){
             fragmentTransaction.commit()
 
         }
+
+        toggle.setOnCheckedChangeListener { _, isChecked ->  //toggles notifications for current user
+            if (isChecked) {
+                user.setUserSubscribedStatus(1)
+            } else {
+                user.setUserSubscribedStatus(0)
+            }
+        }
+
         return view
     }
 
--- app/src/main/java/ie/ul/frankscafe/View/Register.kt
+++ app/src/main/java/ie/ul/frankscafe/View/Register.kt
@@ -16,6 +16,7 @@ import ie.ul.frankscafe.databinding.ActivityMainBinding
 public class Register (val application: Application) : Fragment(R.layout.register) {
     val user = User(9914,"calvin2332","123","pass", 1, 1)
     lateinit var binding: ActivityMainBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater!!.inflate(R.layout.register, container, false)
         val registerButton = view.findViewById<AppCompatButton>(R.id.registerButton)
