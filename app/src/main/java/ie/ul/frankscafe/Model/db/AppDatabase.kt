5f75ac5fe1994dc235f433def9f0e249ae800fba
jackboland <jackboland1201@gmail.com>
2022-12-01 16:02:38 +0000

    Fixed food database with updated schema

--- app/src/main/java/ie/ul/frankscafe/Model/db/AppDatabase.kt
+++ app/src/main/java/ie/ul/frankscafe/Model/db/AppDatabase.kt
@@ -32,7 +32,7 @@ abstract class AppDatabase: RoomDatabase()  {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "app_database"
-                ).build()
+                ).createFromAsset("database/Foods.db").allowMainThreadQueries().build()
                 INSTANCE = instance
                 return instance
             }
