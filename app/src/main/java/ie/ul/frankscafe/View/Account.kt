9527226c1a6110e1198db8fe3dc0df8920a02b39
EvanMcKie <evanmckie@outlook.com>
2022-12-02 14:44:35 +0000

    Push Notification with Button

--- app/src/main/java/ie/ul/frankscafe/View/Account.kt
+++ app/src/main/java/ie/ul/frankscafe/View/Account.kt
@@ -1,11 +1,5 @@
 package ie.ul.frankscafe.View
 
-import android.annotation.SuppressLint
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
-import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,11 +7,12 @@ import android.view.ViewGroup
 import android.widget.Switch
 import android.widget.ToggleButton
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import ie.ul.frankscafe.Model.entity.UserEntity
 import ie.ul.frankscafe.R
+import ie.ul.frankscafe.Services.CurrentUser
+import ie.ul.frankscafe.ViewModel.UserViewModel
 import ie.ul.frankscafe.databinding.ActivityMainBinding
 
 class Account(val application: Application, val mainFragmentManager: MainFragmentManager) : Fragment(R.layout.account){
@@ -30,45 +25,19 @@ class Account(val application: Application, val mainFragmentManager: MainFragmen
         val regButton = view.findViewById<AppCompatButton>(R.id.pastorders)
         val notificationButton = view.findViewById<AppCompatButton>(R.id.notificationButton)
         val toggle = view.findViewById<Switch>(R.id.notificationToggle)
-        //val user = CurrentUser.getuser()
+        val user = CurrentUser.getuser()
 
         regButton.setOnClickListener{
             mainFragmentManager.replaceFragment(PastOrders(application, mainFragmentManager))
         }
 
-        /*toggle.setOnCheckedChangeListener { _, isChecked ->  //toggles notifications for current user
+        toggle.setOnCheckedChangeListener { _, isChecked ->  //toggles notifications for current user
             if (isChecked) {
                 user.setUserSubscribedStatus(1)
             } else {
                 user.setUserSubscribedStatus(0)
             }
-        }*/
-
-        /*notificationButton.setOnClickListener {
-            (activity as MainActivity).showNotification()
-        }*/
-
-/*        @SuppressLint("ServiceCast")
-        fun showNotification(){
-            val channelID = "100"
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-            val builder = NotificationCompat.Builder(applicationContext,channelID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentText("Example Text")
-                .setContentTitle("Example Title")
-
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-                val channel = NotificationChannel(channelID, "Custome Notification", NotificationManager.IMPORTANCE_DEFAULT)
-
-                channel.enableVibration(true)
-                notificationManager.createNotificationChannel(channel)
-                builder.setChannelId(channelID)
-            }
-            val notification = builder.build()
-
-            notificationManager.notify(1000, notification)
-        }*/
+        }
 
         return view
     }
